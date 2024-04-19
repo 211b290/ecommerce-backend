e148623356360a06ef1da2e551373e93860ce060
Shivam Singh <211b290@juetguna.in>
2024-04-19 20:04:38 +0530

    userRoute, sendEmails

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,27 @@
 const express = require("express");
-const {registerUser, loginUser} = require("../controllers/userController");
+const {registerUser, loginUser,logout, forgotPassword, getUserDetails} = require("../controllers/userController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+
+
+
 const router = express.Router();
 
 router.route("/register").post(registerUser);
 
 router.route("/login").post(loginUser);
+
+router.route("/password/forgot").post(forgotPassword);
+
+// impor resetPassword
+// router.route("/password/reset/:token").put(resetPassword);
+
+router.route("/logout").get(logout);
+
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
+
+// import updatePassword
+// router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+
+
 
 module.exports = router;
